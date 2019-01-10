c499bb51232a38df8439dcbd36d76a103fc41d6a
Julien Richard <44161762+JulienYvesRICHARD@users.noreply.github.com>
2019-01-10 11:01:30 +0100

    changing the header from gregoire besson style to the GB-group style, with link and picture, Adding the cookies banner too

---
+++ js/banner.js
@@ -0,0 +1,17 @@
+const cookiesBanner = () => {
+    let cookies = sessionStorage.getItem('user')
+    console.log(cookies)
+    if (cookies != 'cookiesAccepted') {
+        document.getElementById("bandeau-cookies").style.display= "inline"
+    } else {
+        sessionStorage.setItem('user', 'cookiesAccepted');
+    }
+}
+
+cookiesBanner();
+
+
+const infoRemove = () => {
+    sessionStorage.setItem('user', 'cookiesAccepted');
+    document.getElementById("bandeau-cookies").style.display = "none"
+}
